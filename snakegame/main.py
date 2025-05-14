6bed0bcd26c27fc368f259d005cf31043c4792ff
snehamahat211 <snehamahat211@gmail.com>
2025-05-14 15:43:05 +0545

    new folder created

---
+++ snakegame/main.py
@@ -0,0 +1,20 @@
+from turtle import Screen,Turtle
+
+screen=Screen()
+screen.setup(width=600,height=600)
+screen.bgcolor("black")
+screen.title(" our snake game")
+# screen.exitonclick()
+
+starting_position=[(0,0),(-20,0),(-40,0)]
+for position in starting_position:
+    new_segment=Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)
+
+
+
+
+
+
+
