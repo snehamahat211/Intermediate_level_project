c8eaf47b845c13066a9f1afaaedb09d441f937d6
snehamahat211 <snehamahat211@gmail.com>
2025-05-16 18:59:38 +0545

    food and scoreboardmade

--- snakegame/main.py
+++ snakegame/main.py
@@ -1,6 +1,8 @@
 from turtle import Screen
 import time
 from snake import Snake
+from Food import Food
+from scoreboard import Scoreboard
 
 screen=Screen()
 screen.setup(width=600,height=600)
@@ -9,6 +11,8 @@ screen.title(" our snake game")
 screen.tracer(0)
 
 snake=Snake()
+food= Food()
+scoreboard=Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up,"Up")
@@ -22,8 +26,16 @@ game_is_on=True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    #detect collision with food and the distance between food and head
+    if snake.head.distance(food) <15:
+        food.refresh()
+        scoreboard.increase_score()
+
+
+
+
 
 
 screen.exitonclick()
