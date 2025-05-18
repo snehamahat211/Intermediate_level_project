561efbd13af307e65b5ff5d87609abe07acd7aa8
snehamahat211 <snehamahat211@gmail.com>
2025-05-18 15:52:30 +0545

    otherfiles with main created

--- snakegame/main.py
+++ snakegame/main.py
@@ -15,10 +15,10 @@ food= Food()
 scoreboard=Scoreboard()
 
 screen.listen()
-screen.onkey(snake.up,"Up")
-screen.onkey(snake.down,"Down")
-screen.onkey(snake.left,"Left")
-screen.onkey(snake.right,"Right")
+screen.onkey(snake.up,"w")
+screen.onkey(snake.down,"s")
+screen.onkey(snake.left,"a")
+screen.onkey(snake.right,"d")
 
 
 
@@ -31,7 +31,28 @@ while game_is_on:
     #detect collision with food and the distance between food and head
     if snake.head.distance(food) <15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
+
+
+    if snake.head.xcor()>290 or snake.head.xcor()<-290 or snake.head.ycor()>290 or snake.head.ycor()<-290:
+        game_is_on=False
+        scoreboard.game_over()
+
+    #Detect the collision with tall.
+    # for segment in snake.segments:
+    #     if segment ==snake.head:
+    #         pass
+    #     elif snake.head.distance(segment)<10:
+    #         game_is_on=False
+    #         scoreboard.game_over()
+
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment)<10:
+            game_is_on=False
+            scoreboard.game_over()
+
+
 
 
 
